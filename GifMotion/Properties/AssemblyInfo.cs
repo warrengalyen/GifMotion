0eb1f5c13a00bdae06d38562d5676b9b9ac0acc0
Warren Galyen <wgalyen@hotmail.com>
2019-03-28 18:52:38 -0700

    Minor improvements

--- GifMotion/Properties/AssemblyInfo.cs
+++ GifMotion/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GifMotion")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET library for eading and creating animated GIFs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mechanika Design")]
 [assembly: AssemblyProduct("GifMotion")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
